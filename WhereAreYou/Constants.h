647a5b38de6b9b19c3154e85d8a55c3db151009a
Jonas Schmid <jonas.schmid@gmail.com>
2013-05-26 17:39:52 +0200

    Handle position changes from the others

---
+++ WhereAreYou/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  WhereAreYou
+//
+//  Created by Jonas Schmid on 26.05.13.
+//  Copyright (c) 2013 Jonas Schmid. All rights reserved.
+//
+
+#define FB_NAME @"name"
+#define FB_POSITION @"position"
+#define FB_LAT @"lat"
+#define FB_LONG @"long"
+#define FB_ACCURACY @"accuracy"
+#define FB_DATETIME @"datetime"
--- WhereAreYou/ViewController.m
+++ WhereAreYou/ViewController.m
@@ -10,72 +10,134 @@
 
 #import "Firebase/Firebase.h"
 
+#import "Constants.h"
+
 @implementation ViewController {
     Firebase *firebase;
+    Firebase *myself;
+    Firebase *myPosition;
+    
+    NSMutableDictionary *markers;
+    
+    NSString *myName;
+    
+    NSDateFormatter *formatter;
+    NSUserDefaults *prefs;
 }
 
 - (void)viewDidLoad
 {
+    
+    markers = [NSMutableDictionary dictionary];
+    
+    formatter=[[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"dd/MM/yyyy HH:mm"];
+    
+    prefs = [NSUserDefaults standardUserDefaults];
+    
     [super viewDidLoad];
     self.mapView.delegate = self;
     
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    CLLocationCoordinate2D zoomLocation;
-    zoomLocation.latitude = 39.28;
-    zoomLocation.longitude = -76.5;
-    
-    MKPointAnnotation *pos = [[MKPointAnnotation alloc] init];
-    pos.coordinate = zoomLocation;
-    pos.title = @"Hello";
-    pos.subtitle = @"World";
-    
-    [self.mapView addAnnotation:pos];
-    
     
     firebase = [[Firebase alloc] initWithUrl:@"https://whereareyou.firebaseio.com/v1/-InAHfjSC2dI8kpYofrD"];
     
+    NSString *roomName = [firebase name];
+    
+    myName = [prefs stringForKey:roomName];
+    
+    if(myName) {
+        myself = [firebase childByAppendingPath:myName];
+    } else {
+        myself = [firebase childByAutoId];
+        myName = [myself name];
+        [prefs setObject:myName forKey:roomName];
+        [prefs synchronize];
+    }
+    
+    [[myself childByAppendingPath:FB_NAME] setValue:@"iOS l0l"];
+    myPosition = [myself childByAppendingPath:FB_POSITION];
+    
     [firebase observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
-        //        NSLog(@"%@ => %@", snapshot.name, snapshot.value);
-        //        NSLog(@"Has children: %s, %d", snapshot.hasChildren ? "true" : "false", [[snapshot.children allObjects] count]);
-        
-        NSDictionary *value = snapshot.value;
-        
-        NSString *guyName = [value objectForKey:@"name"];
-        
-        NSLog(@"New guy %@", guyName);
-        
-        Firebase *positionRef = [[snapshot ref] childByAppendingPath:@"position"];
-        
-        [positionRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-            NSDictionary *position = snapshot.value;
-            
-            if([position isKindOfClass:[NSNull class]]) {
-                return;
-            }
-            
-            NSLog(@"%@ position: %@", guyName, position);
-            
-            CLLocationCoordinate2D zoomLocation;
-            zoomLocation.latitude = [[position objectForKey:@"lat"] doubleValue];
-            zoomLocation.longitude = [[position objectForKey:@"lon"] doubleValue];
-            
-            MKPointAnnotation *pos = [[MKPointAnnotation alloc] init];
-            pos.coordinate = zoomLocation;
-            pos.title = guyName;
-            pos.subtitle = [position objectForKey:@"datetime"];
-            
-            [self.mapView addAnnotation:pos];
-        }];
-        
+        [self personAddedWithSnapshot:snapshot];
     }];
     
 }
 
+- (void) personAddedWithSnapshot:(FDataSnapshot *)snapshot {
+    NSString *snapshotName = snapshot.name;
+    
+    // Do not capture myself
+    if([snapshotName isEqualToString:myName]) {
+        return;
+    }
+    
+    NSDictionary *value = snapshot.value;
+    
+    NSString *guyName = [value objectForKey:FB_NAME];
+    
+    NSLog(@"New guy %@", guyName);
+    
+    Firebase *positionRef = [[snapshot ref] childByAppendingPath:FB_POSITION];
+    
+    [positionRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        [self positionChangedWithName:guyName andSnapshot:snapshot];
+    }];
+}
+
+- (void) positionChangedWithName:(NSString *)guyName andSnapshot:(FDataSnapshot *)snapshot {
+    NSDictionary *position = snapshot.value;
+    
+    if([position isKindOfClass:[NSNull class]]) {
+        return;
+    }
+    
+    Firebase *ref = [snapshot ref];
+    Firebase *parent = [ref parent];
+    NSString *parentName = [parent name];
+    
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = [[position objectForKey:FB_LAT] doubleValue];
+    zoomLocation.longitude = [[position objectForKey:FB_LONG] doubleValue];
+    
+    NSNumber *dateTimestamp = [position objectForKey:FB_DATETIME];
+    NSTimeInterval interval = [dateTimestamp doubleValue] / 1000.0;
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:interval];
+    NSString *dateString = [formatter stringFromDate:date];
+    
+    MKPointAnnotation *marker = [markers objectForKey:parentName];
+    
+    if(marker) {
+        marker.coordinate = zoomLocation;
+        marker.subtitle = dateString;
+        
+    } else {
+        marker = [[MKPointAnnotation alloc] init];
+        marker.coordinate = zoomLocation;
+        marker.title = guyName;
+        marker.subtitle = dateString;
+    
+        [self.mapView addAnnotation:marker];
+        
+        [markers setValue:marker forKey:parentName];
+    }
+}
+
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
-    [self.mapView setRegion:region animated:YES];
+//    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+//    [self.mapView setRegion:region animated:YES];
+    
+    NSMutableDictionary *newPos = [NSMutableDictionary dictionary];
+    [newPos setValue:[NSNumber numberWithDouble:userLocation.coordinate.latitude] forKey:FB_LAT];
+    [newPos setValue:[NSNumber numberWithDouble:userLocation.coordinate.longitude] forKey:FB_LONG];
+    [newPos setValue:[NSNumber numberWithDouble:userLocation.location.horizontalAccuracy] forKey:FB_ACCURACY];
+    
+    NSNumber *date = [NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000.0)];
+    [newPos setValue:date forKey:FB_DATETIME];
+    
+    [myPosition setValue:newPos];
 }
 
 @end
