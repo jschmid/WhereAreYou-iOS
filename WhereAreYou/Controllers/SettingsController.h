42c3c3f076c5653b77b656e2e3aea9e655a1e6ad
Jonas Schmid <jonas.schmid@gmail.com>
2013-05-27 10:53:59 +0200

    Added the settings controller Moved the Controllers to their own folder

---
+++ WhereAreYou/Controllers/SettingsController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsController.h
+//  WhereAreYou
+//
+//  Created by Jonas Schmid on 5/27/13.
+//  Copyright (c) 2013 Jonas Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+
+@end
---
+++ WhereAreYou/Controllers/SettingsController.m
@@ -0,0 +1,38 @@
+//
+//  SettingsController.m
+//  WhereAreYou
+//
+//  Created by Jonas Schmid on 5/27/13.
+//  Copyright (c) 2013 Jonas Schmid. All rights reserved.
+//
+
+#import "SettingsController.h"
+
+@interface SettingsController ()
+
+@end
+
+@implementation SettingsController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
